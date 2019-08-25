20202b2c24494f617e8c99352bd16695a7ac567b
ofrot@microsoft.com <ofrot@microsoft.com>
2019-08-25 19:56:37 +0300

    TypeScriptSchemaGenerator and a sample

---
+++ src/Samples/Samples.RulesBasedTypeScriptSchemaGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using SchemaGenerator.Common;
+using SchemaGenerator.Samples.Shape;
+using SchemaGenerator.TypeScript;
+using System;
+using System.Reflection;
+
+namespace SchemaGenerator.Samples.RulesBasedTypeScriptSchemaGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var schemaGenerator =
+                new TypeScriptSchemaGenerator(
+                    new[] { typeof(Polygon) },
+                    _ => _.Name.StartsWith("SchemaGenerator.Samples"),
+                    _ =>
+                    {
+                        switch (_)
+                        {
+                            case FieldInfo fieldInfo:
+                                return fieldInfo.IsPublic;
+                            case PropertyInfo propertyInfo:
+                                return propertyInfo.GetMethod != null && propertyInfo.SetMethod != null;
+                            default:
+                                throw new UnexpectedException(nameof(MemberInfo), _.GetType().Name);
+                        }
+                    });
+            schemaGenerator.Validate();
+            var schema = schemaGenerator.Generate();
+
+            Console.WriteLine(schema);
+        }
+    }
+}
---
+++ src/TypeScript/TypeScriptSchemaGenerator.cs
@@ -0,0 +1,97 @@
+﻿using MoreLinq;
+using SchemaGenerator.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TypeLiteTypeScript = TypeLite.TypeScript;
+
+namespace SchemaGenerator.TypeScript
+{
+    public sealed class TypeScriptSchemaGenerator : SchemaGenerator
+    {
+        private readonly string _shortcutSchemaName;
+
+        public TypeScriptSchemaGenerator(
+            IReadOnlyCollection<Type> rootTypes,
+            Func<AssemblyName, bool> isInScope,
+            Func<MemberInfo, bool> shouldSerializeMember,
+            string shortcutSchemaName = "Schema")
+        : base(
+            rootTypes,
+            isInScope,
+            shouldSerializeMember)
+        {
+            _shortcutSchemaName = shortcutSchemaName;
+        }
+
+        protected override string Generate(IReadOnlyCollection<Type> types)
+        {
+            var typeScriptFluent =
+                TypeLiteTypeScript.Definitions().
+                    WithShouldIgnoreMemberPredicate(_ => !ShouldSerializeMember(_)).
+                    WithShouldIncludeAllEnums(false).
+                    AsCollectionDictionaries(false).
+                    AsConstEnums().
+                    AsStringEnums();
+
+            typeScriptFluent.WithMemberTypeFormatter(
+                (tsProperty, memberTypeName) =>
+                    tsProperty.PropertyType.Type.HasAttribute<FlagsAttribute>()
+                        ? $"{memberTypeName}[]"
+                        : typeScriptFluent.ScriptGenerator.DefaultMemberTypeFormatter(tsProperty, memberTypeName));
+
+            var serializableTypes =
+                types.
+                    Where(_ => !typeof(IEnumerable).IsAssignableFrom(_)).
+                    ToList();
+
+            serializableTypes.ForEach(_ => typeScriptFluent.For(_));
+
+            return $"{typeScriptFluent.Generate().TrimStart().Replace("\t", "    ")}" +
+                   $"{GenerateImports(serializableTypes)}";
+        }
+
+        protected override void Validate(IReadOnlyCollection<Type> types)
+        {
+            Ensure.NotNull(nameof(types), types);
+
+            var duplicateNameTypes =
+                types.
+                    Distinct().
+                    GroupBy(_ => _.Name).
+                    Where(_ => _.Count() > 1).
+                    Select(_ => _.Key).
+                    ToList();
+            if (_shortcutSchemaName != null && duplicateNameTypes.Any())
+            {
+                throw new ExtendedException(
+                    "Types with duplicate name are not supported with shortcut schema name option [" +
+                    $"{nameof(duplicateNameTypes)}={duplicateNameTypes.ToJoinString()}]");
+            }
+        }
+
+        private string GenerateImports(IReadOnlyCollection<Type> types)
+        {
+            if (_shortcutSchemaName == null)
+            {
+                return "";
+            }
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"declare namespace {_shortcutSchemaName} {{");
+            types.ToDictionary(
+                _ => 
+                     _.IsGenericType
+                         ? _.Name.Split("`", 2).
+                             First()
+                         : _.Name).
+                OrderBy(_ => _.Key).
+                ForEach(_ => stringBuilder.AppendLine($"    export import {_.Key} = {_.Value.Namespace}.{_.Key};"));
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+    }
+}
